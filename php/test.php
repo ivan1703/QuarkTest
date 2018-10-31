b02ec398f2f3abacd8d8c14ff4c77f8ca4fa3e77
ivan1703 <iwan.balaschow228@mail.ru>
2018-10-31 16:46:09 +0400

    Three commit добавил форму

--- php/test.php
+++ php/test.php
@@ -1,2 +1,40 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>уроки php</title>
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="../css/bootstrap.css" rel="stylesheet" id="bootstrap-css">
+</head>
+<body>
+<h1 class="h1"> На этой странице я учу пэхэпэ</h1>
+<form class = "container" name = "reg" method="post">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+  </div>
+  <br>
+  <button type="submit" class="btn btn-primary" name = "input"> Войти </button>
+</form>
+</body>
+</html>
+<?php
+
+// gettype = функция для проверки типа данных 
+//unset = уничтожить переменную 
+//define() = создает константу
+//print_r(<per>) - распечсатывает переменную
+
+if (isset($_POST['input'])){
+  
+}
+
 ?>
